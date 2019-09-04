a0961f4d44d9432596b06b2984e27d32b55a5b92
apresta <presta.alberto@libero.it>
2019-09-04 12:22:33 +0200

    M/D/1 corretta

--- triangular_service.py
+++ triangular_service.py
@@ -37,32 +37,65 @@ def PSRA_G(lasso_temporale_in_ore,distribuzione,fattore_sigma):
 # questa simulazione invece il service time è deterministico, però gli arrivi
 # non seguono più il PSRA ma sono arrivi che si basano su Poisson dix
 
-def M_D_1(lam,mu,max_time):
-    "lam = mean of arrival of airctraft (poisson dix)"
-    " mu = frequency of service time (deterministic)"
-    " max_time = considered time_lapse"
-    T = max_time*60*60
-    N=int(T*mu)
-    arrival_poisson = rm.expovariate(lam)
-    arrival = np.zeros(N)
-    arrival[0] = arrival_poisson
-    for i in range(1,N):
-        arrival_poisson = rm.expovariate(lam)
-        arrival[i] = arrival[i-1] + arrival_poisson
-    queue = np.zeros(N)
-    for i in range(1,N):
-        arrival_in_slot = len(arrival[(arrival>=90*(i-1)) & (arrival<90*i)])
-        if i>1:
-            queue[i]=queue[i-1]+arrival_in_slot-int(queue[i-1]!=0)
+def M_D_1(arrival_time,max_time,service_time=1/90):
+    """
+    arrival_time = frequency of the exponential distribution related to the arrival time in 1/seconds
+    service_time = deterministic service time in 1/seconds
+    max_time = time of simulation in hour to be converted in seconds in the program
+
+    OUTPUT: arrivi e vettore della coda
+    """
+    #conversion in seconds
+    max_seconds = max_time*60*60
+    sim_time = 0.0 # simulation time
+    t_1 = 0.0 # time for next event (arrival)
+    t_2 = max_seconds # time for next event (departure)
+    t_n = 0.0 #last event time--> tempo dell'ultimo avvenimento generico
+    t_b = 0.0 # last start of busy time--> tempo in cui la queue inizia ad essere non vuota per l'ultima volta
+    c = 0 # numero di servizi completati
+    queue_aircraft = [] # number of aircraft in the queue
+    aircraft = 0
+    arrival = [] # time of arrival
+    attesa = [] # attesa per gli aerei-->NON SICURO CHE SI CALCOLI COSI'
+    # simulation loop
+    while(sim_time < max_seconds):
+        if(t_1<t_2): #event1:arrival
+            sim_time = t_1
+            arrival.append(t_1)
+            aircraft += 1
+            queue_aircraft.append(aircraft)
+            t_n = sim_time
+            t_1 = sim_time + rm.expovariate(arrival_time)
+            if(aircraft==1):
+                t_b = sim_time
+                t_2 = sim_time + 1/service_time
         else:
-            queue[i]=queue[i-1]+arrival_in_slot-1
+            sim_time = t_2
+            aircraft = aircraft -1
+            queue_aircraft.append(aircraft)
+            t_n = sim_time
+            attesa.append( t_2 - arrival[c])
+            c+=1
+            if(aircraft>0):
+                t_2=sim_time + 1/service_time
+            else:
+                t_2 = max_seconds
 
 
-    return queue,arrival
+
+
+    return queue_aircraft,arrival,attesa
+
 
 lam = 1/90
-a,b = M_D_1(1/83,1/90,3)
-max(a)
+a,b,c,thrpughput = M_D_1(1/90,1)
+thrpughput
+
+for i in range(len(c)):
+    c[i]=c[i]/90
+
+c
+
 def create_distribution(v):
     "funzione che crea una distribuzione discreta di probabilità"
     "serve per avere la distribuzione sulle possibili lunghezze della queue"
@@ -91,6 +124,7 @@ for i in range(12):
     domain.append(i)
 x = create_distribution(queue_u)
 y = create_distribution(a)
-plt.plot(x,label = "PRSA")
+plt.plot(x,label = "PRSA",)
 plt.plot(y,label = "M/D/1")
+plt.grid()
 plt.legend()

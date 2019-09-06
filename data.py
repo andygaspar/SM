8b2a4e79aa850b300a82ba2bc994e61df88e7923
Andrea Gasparin <andrea.gasparin.87@gmail.com>
2019-09-06 22:29:22 +0200

    primi conti e riordino

--- data.py
+++ data.py
@@ -173,3 +173,178 @@ while i < df_entry_day1.shape[0]-1:
         i+=1
     print(i)
 df_traj
+
+"""
+************************parte albi
+"""
+
+
+
+#creazione dataframe con ultimi waypoint
+df_traj=pd.DataFrame(columns=['flight','wp1','wp2','wp3','wp4','wp5','t1','t2','t3','t4','t5'])
+
+
+df_entry_day1
+export_df = df_entry_day1.to_csv(r'../data/file.csv',index = None, header=True)
+
+names=['flight','wp1','wp2','wp3','wp4','wp5','t1','t2','t3','t4','t5']
+i=0
+while i < df_entry_day1.shape[0]-1:
+    row=[df_entry_day1.iloc[i]["ifps_id"],'None','None','None','None','None','None','None','None','None','None']
+    j=1
+    cont = 0 #contare quante volte si entra
+    while (df_entry_day1.iloc[i]["ifps_id"]==df_entry_day1.iloc[i+1]["ifps_id"] and j<=4):
+        j+=1
+        cont = cont + 1
+    if cont ==0:
+        print("A SHIT")
+        i = i+ 1
+        continue
+    for k in range(j):
+        row[1+k]=df_entry_day1.iloc[i+j-k-1]["sid"]
+        row[1+k+5]=df_entry_day1.iloc[i+j-k-1]["time"]
+    df_traj=df_traj.append(dict(zip(names,row)),ignore_index = True)
+    print("**********************************************")
+    for m in range(len(row)):
+        print(row[m])
+    print("**********************************************")
+    i+=j
+    while (i<=df_entry_day1.shape[0]-2 and df_entry_day1.iloc[i]["ifps_id"]==df_entry_day1.iloc[i+1]["ifps_id"]):
+        i+=1
+    print("questa è la j:",j)
+    #if(j>1):
+    i+=1
+    print(i)
+df_traj
+#ORA   : prendo il dataset relativo agli arrivi e lo pulisco nel modo seguente:
+#   -considero gli arrivi solo a Francoforte cancellando le altre righe
+#   -cancello la colonna relativa all'origine (inutile per noi)
+#   -metto a posto le colonne relative al timing (divido data e ora) e considero solo la nostra dataset
+
+#dopo di chè dovrò creare la colonna arrivi
+
+df_ar
+
+#condizione di arrivo a Francoforte-->nuovo dataset con Francoforte
+Frankfurt_cond = df_ar["D"]=="EDDF"
+df_ar_frank = df_ar[Frankfurt_cond]
+df_ar_frank
+
+#metto a posto arr_time
+arr_date = []
+arr_land = []
+for i in range(df_ar_frank.shape[0]):
+    temp = df_ar_frank.iloc[i]["arr_time"]
+    temp=temp.split(" ")
+    arr_date.append(temp[0])
+    arr_land.append(temp[1])
+
+
+df_ar_frank["date"]=arr_date
+df_ar_frank["time"]=arr_land
+
+df_ar_frank= df_ar_frank.drop(columns=["O","arr_time"])
+
+df_ar_frank
+
+#considero solo i voli in data 2017-09-13
+date_condition = df_ar_frank["date"]=="2017-09-13"
+df_ar_frank_day1 = df_ar_frank[date_condition]
+
+
+df_ar_frank_day1
+
+#creiamo file csv di questi arrivi
+arr_frankfurt_13_09_17 = df_ar_frank_day1.to_csv(r'../data/arr_frankfurt_13_09_17.csv',index = None, header=True)
+
+
+arrivi = [] # non so se serve però intanto lo metto
+voli = []
+#ora la parte più difficile
+for i in range(df_traj.shape[0]):
+    flight = df_traj.iloc[i]["flight"]
+    cont = 0
+    for j in range(df_ar_frank_day1.shape[0]):
+        aereo = df_ar_frank_day1.iloc[j]["ifps_id"]
+        if(aereo==flight):
+            cont = 1
+            temp = df_ar_frank_day1.iloc[j]["time"]
+            arrivi.append(temp)
+    if(cont==0):
+        arrivi.append("None")
+
+    print(cont)
+
+
+#aggiungo la colonna
+len(arrivi)e={"a":1}
+"a"in e
+
+
+df_traj["arrival"] = arrivi
+
+
+df_traj
+
+
+"""
+*****************************************************************
+aggiunta funzioni sul contteggio degli waypoint
+
+è una lista con 5 dizionari ogniuno riferito ad
+un wp, ordinati per numero di passaggi
+"""
+
+
+
+wp_dict_1={}
+wp_dict_2={}
+wp_dict_3={}stat_waypoint[4].key[0]
+wp_dict_4={}
+wp_dict_5={}
+stat_waypoint=[wp_dict_1,wp_dict_2,wp_dict_3,wp_dict_4,wp_dict_5]
+
+
+names=["wp1","wp2","wp3","wp4","wp5"]
+for j in range(5):
+    for i in range(df_traj.shape[0]):
+        way_p=df_traj.iloc[i][names[j]]
+        if way_p in stat_waypoint[j]:
+            stat_waypoint[j][way_p]+=1
+        else:
+            stat_waypoint[j][way_p]=1
+
+    #riordino dizionario
+    stat_waypoint[j]=sorted(stat_waypoint[j].items(), key =lambda kv:(kv[1], kv[0]),reverse=True)
+    stat_waypoint[j]=dict(stat_waypoint[j])
+stat_waypoint[0]
+'REDNI' in stat_waypoint[0]
+
+
+
+
+"""
+*********************************************************************
+calcolo tempi arrivo (da fare)
+"""
+df_traj
+def time_to_sec(arr):
+    arr=arr.replace(":"," ")
+    arr=arr.split(" ")
+    arr=int(arr[0])*3600  +  int(arr[1])*60   +  int(arr[2])
+    return arr
+
+def percorrenza(entry,arr):
+    return time_to_sec(arr)-time_to_sec(entry)
+
+
+#lista percorrenza voli da SIRPO
+def perc_per_waypoint(waypoint):
+    perc=[]
+    for i in range(df_traj.shape[0]):
+        if df_traj.iloc[i]["wp1"]==waypoint:
+            perc.append(percorrenza(df_traj.iloc[i]["t1"],df_traj.iloc[i]["arrival"]))
+    return perc
+a=perc_per_waypoint('SIRPO')
+a=np.array(a)
+np.mean(a)

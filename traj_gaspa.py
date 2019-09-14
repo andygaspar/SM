b813175f6bb8c12710e77dbd24cd05d276e4a9b9
apresta <presta.alberto@libero.it>
2019-09-14 15:04:07 +0200

    tentativi

--- traj_gaspa.py
+++ traj_gaspa.py
@@ -63,3 +63,121 @@ def shift_coor(d_wp_coor):len(traj_list)
 
 scaled_long,scaled_lat=shift_coor(d_wp_coor)
 plt.scatter(scaled_long,scaled_lat)
+
+
+
+
+
+"""
+CREAZIONE WP_COORD
+"""
+df.shape[0]
+cc = df.iloc[0]["coor"]
+cc=cc.replace("POINT","")
+cc
+wp_coord = {}
+
+for i in range(df.shape[0]):
+    s = df.iloc[i]["sid"]
+    coor = df.iloc[i]["coor"]
+    coor = coor.replace("POINT","")
+    coor = coor.replace("(","")
+    coor = coor.replace(")","")
+    coor = coor.split(" ")
+    if(s not in wp_coord):
+        wp_coord[s]=coor
+
+
+len(trajectory)
+
+
+"""
+calcolo la distanza per ogni coppia
+"""
+
+
+
+def distanza_coppie(traj = trajectory,dict_wp = wp_coord):
+    res = {}
+    for i in range(len(traj)):
+        traiettoria = traj[i]
+        if len(traiettoria)==1:
+            continue
+        else:
+            for j in range(len(traiettoria)-1):
+                p1 = traiettoria[j]
+                p2 = traiettoria[j+1]
+                stringa = p1+"-"+p2
+                c1 = wp_coord[p1]
+                c2 = wp_coord[p2]
+                dist=geodesic(c1,c2).kilometers
+                if(stringa not in w):
+                    res[stringa]=dist
+    return res
+
+dictionario_distanza_coppie = distanza_coppie()
+
+
+
+
+
+
+pr = trajectory[0]
+df
+
+cont = 0
+for i in range(len(pr)-1):
+    p1 = df.iloc[i]
+    p2=df.iloc[i+1]
+    t1 = p1["time_sec"]
+    t2 = p2["time_sec"]
+    cont += t2-t1
+
+cont = df.iloc[len(pr)-1]["time_sec"] - df.iloc[0]["time_sec"]
+
+cont
+#1029
+
+def tempo_di_percorrenza_percorsi(df = df,traj = trajectory):
+    res = []
+    i = 0
+
+    lunghezza = 0
+    for cont in range(len(traj)):
+        temp = traj[cont]
+        lunghezza = len(temp) - 1
+        if len(temp)==1:
+            res.append(0)
+            i = i+1+lunghezza
+        
+        else:
+            tp = df.iloc[lunghezza + i]["time_sec"] - df.iloc[i]["time_sec"]
+            res.append(tp)
+        i = i+1+lunghezza
+
+    return res
+
+
+q = tempo_di_percorrenza_percorsi()
+len(q)
+len(trajectory)
+def merge_percorsi_tempi(traj = trajectory,df = df):
+    tempi = tempo_di_percorrenza_percorsi(df,traj)
+    res = {}
+    for i in range(len(traj)):
+        #trasformo i percorsi in stringhe
+        traiettoria = traj[i]
+        s = ""
+        for j in range(len(traiettoria)):
+            if(j<len(traiettoria)-1):
+                s = s+traiettoria[j]+"-"
+            else:
+                s = s+traiettoria[j]
+        print(s)
+        if(s not in res):
+            res[s]=tempi[i]
+    return res
+
+
+
+pp = merge_percorsi_tempi()

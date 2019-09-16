9512f1f09bcd8a6a5e676686bea9260d41e054ee
apresta <presta.alberto@libero.it>
2019-09-16 14:17:24 +0200

    minimi_tempi_su_traiettorie

--- traj_gaspa.py
+++ traj_gaspa.py
@@ -8,7 +8,10 @@ import csv
 import copy
 
 
-df=pd.read_csv("../data/completo.csv")
+
+df
+
+df=pd.read_csv("../data/completo.csv") # DA AGGIUNGERE : matcho con arrivo l'aereo con sid aereoporto
 df_ar = pd.read_csv("../data/arrivi_1709.csv")
 df=data.dist_filter(df,200)
 lista_date,lista_wp,lista_freq_wp,wp_coor=data.carica_liste()
@@ -18,33 +21,102 @@ df
 
 main=["KERAX","PSA","ROLIS","UNOKO"]
 
-
+df_ar
 #lavoro solo su una data
+fr_cond = df_ar["D"] =="EDDF"
+df_ar = df_ar[fr_cond]
+ data.rinomina(df_ar,"ifps_id","aereo")
+data.data_time(df_ar,"arr_time")
+data.rinomina(df_ar,"ac_id","sid")
+for i in range(df_ar.shape[0]):
+    df_ar.iloc[i]["sid"] = "AIRPORT"
+
+df_ar
+
+
+df
+"""
+idea aggiungere arrivo al df completo
+"""
+lista = ["aereo","sid","coor","distance","date","time","time_sec"]
+
 
 
 
 """
-"""
+PARTE DI PREPROCESSING DI DF_ARR
 
+"""
+df_ar
+#lavoro solo su una data
+fr_cond = df_ar["D"] =="EDDF"
+df_ar = df_ar[fr_cond]
+ data.rinomina(df_ar,"ifps_id","aereo")
+data.data_time(df_ar,"arr_time")
+data.rinomina(df_ar,"ac_id","sid")
+for i in range(df_ar.shape[0]):
+    df_ar.iloc[i]["sid"] = "AIRPORT"
+tempi = []
+for i in range(df_ar.shape[0]):
+    t = df_ar.iloc[i]["time"]
+    p = fun.time_to_sec(t)
+    tempi.append(p)
+df_ar
+
+tempi
+df_ar["time_sec"]=tempi
+df
 
 df_ar
 df_ar =data.data_time(df_ar,"arr_time")
 cond = df_ar["date"]=='2017-08-17'
 df_ar =df_ar[cond]
 df_ar
+df_ar["coor"]="POINT(50.037753 8.560964)"
+df_ar["distance"]=0
+df_ar = df_ar.drop(["D","O"],axis=1)
+df_ar = df_ar.drop(["arr_time"],axis=1)
+df_ar
+df
+
+def complete_dataframe(df = df,df_ar = df_ar, lista = lista):
+    """
+    INPUT : come input ci voglione due dataframe, il primo relativo ai passaggi nei
+        waypoints, metre il secondo relativo agli arrivi
+        il terzo input è una lista con i nomi delle colonne dei due dataframe, che ripeto
+        DEVONO ESSERE UGUALI
+    ATTENZIONE: siccome questa funzione si basa su appendere diversi dataframe,
+                questi due devono avere le stesse colonne con lo stesso nome e lo stesso
+                numero...questo comporta che prima il dataframe relativo agli arrivi
+                debba essere modificato in tal senso
+    OUTPUT: dataframe del percorso completo
+    """
+    #fase 1--> creare dict con tutti i voli e indice realtivo al daframe df
+    df_totale = pd.DataFrame(columns = ["aereo","sid","coor","distance","date","time","time_sec"])
+    pr = {}
+    i = 0
+    while(i<df.shape[0]):
+        aer = df.iloc[i]["aereo"] #salvato l'aereo
+        pr[aer] = i
+        j=i+1
+        while(j<df.shape[0]-1 and df.iloc[j]["aereo"]==aer ):
+            j = j+1
+        i = j
+    lista_indici = list(pr.values())
+    #fase due quello che dobbiamo fare
+    for voli in pr:
+        cond = df["aereo"]==voli
+        temp = df[cond]
+        #ora lavoro sui daframe temp
+        cond_ar = df_ar["aereo"]==voli
+        temp_arrival = df_ar[cond_ar]
+        temp = temp.append(temp_arrival)
+        df_totale = df_totale.append(temp)
+    return df_totale
 
 
-
-
-
-
-trajectory=traiettorie_complete(df)
-trajectory[1]
-
-
-
-
-
+df_tot = complete_dataframe()
+df_tot
 def traiettorie_complete(df):
     """
         INPUT:
@@ -65,7 +137,17 @@ def traiettorie_complete(df):
     return traj_list
 
 
-df
+
+
+
+trajectory=traiettorie_complete(df_tot)
+trajectory[5]
+
+
+
+
+
+
 
 
 def shift_coor(d_wp_coor):
@@ -98,34 +180,34 @@ plt.scatter(scaled_long,scaled_lat)
 
 
 """
-CREAZIONE WP_COORD--> da funzionalizzare
+CREAZIONE WP_COORD
 """
-df.shape[0]
-cc = df.iloc[0]["coor"]
-cc=cc.replace("POINT","")
-cc
-wp_coord = {}
-
-for i in range(df.shape[0]):
-    s = df.iloc[i]["sid"]
-    coor = df.iloc[i]["coor"]
-    coor = coor.replace("POINT","")
-    coor = coor.replace("(","")
-    coor = coor.replace(")","")
-    coor = coor.split(" ")
-    if(s not in wp_coord):
-        wp_coord[s]=coor
 
 
-len(trajectory)
+def dict_coordinate(df = df):
+    """
+    INPUT: dataframe con informazioni sui voli
+    OUTPUT: dizionario dove per ogni waypoint vengono associate le coordinate
+    """
+    wp_coord = {}
+    for i in range(df.shape[0]):
+        s = df.iloc[i]["sid"]
+        coor = df.iloc[i]["coor"]
+        coor = coor.replace("POINT","")
+        coor = coor.replace("(","")
+        coor = coor.replace(")","")
+        coor = coor.split(" ")
+        if(s not in wp_coord):
+            wp_coord[s]=coor
+    center = ["50.037753","8.560964"]
+    wp_coord["AIRPORT"]=center
+    return wp_coord
+wp_coord = dict_coordinate()
 
-
+wp_coord
 """
 calcolo la distanza per ogni coppia
 """
-
-
-
 def distanza_coppie(traj = trajectory,dict_wp = wp_coord):
     """
     INPUT:
@@ -156,12 +238,8 @@ def distanza_coppie(traj = trajectory,dict_wp = wp_coord):
 
 dictionario_distanza_coppie = distanza_coppie()
 
-dictionario_distanza_coppie["MTR-ORVIV"]
-trajectory
-len(trajectory)
 
 dictionario_distanza_coppie
-
 
 
 
@@ -181,24 +259,9 @@ def trovo_ripetizioni(perc,tr = trajectory):
     return res
 
 
-ì
+df_tot
 
-len(trajectory)
-
-cont = 0
-for i in range(len(trajectory)):
-    tr = trajectory[i]
-    for j in range(len(tr)-1):
-        if(tr[j]=="MTR" and tr[j+1]=="ORVIV"):
-            print(i)
-            print(tr)
-            cont+=1
-            print("************************************************")
-
-cont
-df
-
-def minimo_coppia(wp1,wp2,df=df):
+def minimo_coppia(wp1,wp2,df=df_tot):
     """
     auxiliary function che calcola, dati due waypoint, il minimo tempo per
     passare dal primo al secondo
@@ -214,12 +277,12 @@ def minimo_coppia(wp1,wp2,df=df):
                 print(i)
     return min
 
-dictionario_distanza_coppie = distanza_coppie()
+
 lista_coppie = list(dictionario_distanza_coppie.keys())
 lista_coppie
-dictionario_distanza_coppie["MTR-ORVIV"]
 
-def tempi_minimi_coppie( lista_coppie = lista_coppie, df = df):
+
+def tempi_minimi_coppie( lista_coppie = lista_coppie, df = df_tot):
     """
     INPUT :
         -lista_coppie = lista con tutte le coppie sotto forma di stringa
@@ -244,7 +307,7 @@ tempi_coppie["MTR-ORVIV"]
 trajectory
 
 
-def minimo_percorsi(tempi_coppie = tempi_coppie,df = df,traj = trajectory):
+def minimo_percorsi(tempi_coppie = tempi_coppie,df = df_tot,traj = trajectory):
     """
     INPUT:
         - tempi_coppie = dictionary con i minimi tempi per ogni coppia
@@ -274,4 +337,4 @@ def minimo_percorsi(tempi_coppie = tempi_coppie,df = df,traj = trajectory):
     return res
 
 min_tempi_percorsi = minimo_percorsi()
-len(min_tempi_percorsi)
+min_tempi_percorsi
